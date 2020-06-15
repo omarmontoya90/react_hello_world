50e970fefd0a670c79fd55d23ee8dfa977229cb8
omar montoya <montoyaomar01@hotmail.com>
2020-06-14 20:35:09 -0500

    form to create a component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+//function HelloWorld(props) {
+//  return <h2>{props.title}</h2>
+//}
+
+//const HelloWorld = (props) => <h2>{props.title}</h2>
+
+class HelloWorld extends Component {
+  render(){
+    return <h2>{this.props.title}</h2>
+    };
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>This is my Hello World</h2>
+        <HelloWorld title="This is my Hello World"/>
         <p>Hello World, I am using React</p>
       </header>
     </div>
